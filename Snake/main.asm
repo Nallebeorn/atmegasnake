3da83267c109ca967c2f4a61ba29ea31107bda32
Benjamin <mafortion.benjamin@gmail.com>
2020-05-18 17:22:12 +0200

    Initial

---
+++ Snake/main.asm
@@ -0,0 +1,48 @@
+.DEF rTemp         = r16
+.DEF rDirection    = r23
+
+.EQU NUM_COLUMNS   = 8
+.EQU MAX_LENGTH    = 25
+
+
+.DSEG
+
+matrix:   .BYTE 8
+snake:    .BYTE MAX_LENGTH+1
+
+.CSEG
+// Interrupt vector table
+.ORG 0x0000
+     jmp init  // Reset
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp 0
+     jmp timer  // Timer 0 overflow
+
+init:
+     // Sätt stackpekaren till högsta minnesadressen
+     ldi	rTemp, HIGH(RAMEND)
+     out	SPH, rTemp
+     ldi	rTemp, LOW(RAMEND)
+     out	SPL, rTemp
+
+	 sbi	PORTC, PORTC0
+	 sbi	PORTD, PORTD6
+	 sbi	PORTD, PORTD7
+	 ldi	rTemp, 0x3f
+	 out	PORTB, rTemp
+
+timer:
+	jmp	timer
